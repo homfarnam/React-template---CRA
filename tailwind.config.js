d831bd90da515fee95798de12472b2156f4e6fe1
homfarnam <homfarnam@gmail.com>
2021-09-17 22:14:19 +0430

    chore(config): Add purge config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,26 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
+  // Uncomment the line below to enable the experimental Just-in-Time ("JIT") mode.
+  // https://tailwindcss.com/docs/just-in-time-mode
+  mode: "jit",
   theme: {
     extend: {},
   },
-  variants: {
-    extend: {},
-  },
+  variants: {},
   plugins: [],
+  purge: {
+    // Filenames to scan for classes
+    content: [
+      "./src/**/*.html",
+      "./src/**/*.js",
+      "./src/**/*.jsx",
+      "./src/**/*.ts",
+      "./src/**/*.tsx",
+      "./public/index.html",
+    ],
+    // Options passed to PurgeCSS
+    options: {
+      // Whitelist specific selectors by name
+      // safelist: [],
+    },
+  },
 }
